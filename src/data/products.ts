7bfbda1c1d3ace7cf5f836cab061d8e2a236f1f3
ulisesjalos <156099603+ulisesjalos@users.noreply.github.com>
2026-05-14 11:12:22 -0600

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -32,7 +32,7 @@ export const productosPlayeras: Producto[] = [
     genero: 'Juvenil',
     nombre: 'PLAYERA PARA JOVEN',
     precios: { base: 100, XXG: 120 },
-    imagen: 'https://images.pexels.com/photos/5698855/pexels-photo-5698855.jpeg?auto=compress&cs=tinysrgb&w=600',
+    imagen: 'https://plus.unsplash.com/premium_photo-1707816501388-6030b9f3eee8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2hpcnQlMjB0ZWVuYWdlfGVufDB8fDB8fHww',
   },
   {
     id: 'N0300',

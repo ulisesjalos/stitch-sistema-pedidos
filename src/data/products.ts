dec12bab62f398b4b7f4c66010080824b435e361
ulisesjalos <156099603+ulisesjalos@users.noreply.github.com>
2026-05-15 10:23:23 -0600

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -223,7 +223,7 @@ export const productosSudaderas: Producto[] = [
     genero: 'Unisex',
     nombre: 'SUDADERA CAPUCHA Y CANGURERA',
     precios: { base: 300, XXG: 320 },
-    imagen: 'https://www.moplayeras.com/cdn/shop/products/91772_MD_d608ebf7-2028-46a2-993b-8ae60724e4d0_720x.jpg?v=1682109759',
+    imagen: 'https://passionpromocional.com/cdn/shop/products/Diapositiva10_e1c3de1f-f19a-4203-9583-3cb2b784601a_800x.jpg?v=1628981233',
   },
   {
     id: 'C0702',

5854a14944fd81de975a0136eaf802b08870ef13
ulisesjalos <156099603+ulisesjalos@users.noreply.github.com>
2026-05-15 16:21:58 -0600

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -26,8 +26,8 @@ export const productosPlayeras: Producto[] = [
   precios: { base: 120, XXG: 140 },
   // REGLA PARA DOBLE FOTO:
   imagenes: [
-    "https://tu-sitio.com/playera-frente.jpg", // Foto 1 (Se ve al inicio)
-    "https://tu-sitio.com/playera-espalda.jpg" // Foto 2 (Se ve al pasar el mouse)
+    "https://www.moplayeras.com/cdn/shop/files/91931_MF_1_720x.png?v=1682376996", // Foto 1 (Se ve al inicio)
+    "https://www.moplayeras.com/cdn/shop/products/92233_MF_720x.jpg?v=1682367230" // Foto 2 (Se ve al pasar el mouse)
   ],
   // ... el resto de tus campos
 },

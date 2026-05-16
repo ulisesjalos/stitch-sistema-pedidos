93357378ff3d91d5b07c5e55176901392a44b055
ulisesjalos <156099603+ulisesjalos@users.noreply.github.com>
2026-05-15 20:57:41 -0600

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -26,7 +26,7 @@ export const productosPlayeras: Producto[] = [
     precios: { base: 120, XXG: 140 },
     imagen: 'https://www.moplayeras.com/cdn/shop/products/92233_MF_720x.jpg?v=1682367230',
     imagenes: [
-      'https://https://secuenciasolutions.com.mx/2020/wp-content/uploads/2024/08/PLAYERA-ADULTO-MampO-MOD.-4800-COLORES-COLORES.png',
+      'https://www.moplayeras.com/cdn/shop/products/92233_MF_720x.jpg?v=1682367230',
       'https://https://secuenciasolutions.com.mx/2020/wp-content/uploads/2024/08/PLAYERA-ADULTO-MampO-MOD.-4800-COLORES-COLORES.png'
     ]
   },

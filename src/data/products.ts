3818549bf3a00487cd5ffacc0444378a8d74af53
ulisesjalos <156099603+ulisesjalos@users.noreply.github.com>
2026-05-16 13:42:30 -0600

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -64,7 +64,7 @@ export const productosPlayeras: Producto[] = [
     imagen: 'https://www.moplayeras.com/cdn/shop/products/92233_MF_720x.jpg?v=1682367230',
     imagenes: [
       'https://www.moplayeras.com/cdn/shop/products/92233_MF_720x.jpg?v=1682367230',
-      '/polo.jpg'
+      'https://secuenciasolutions.com.mx/2020/wp-content/uploads/2024/08/PLAYERA-ADULTO-MampO-MOD.-4800-COLORES-COLORES.png'
     ],
     colores: COLORES_C0300_CUSTOM
   },
